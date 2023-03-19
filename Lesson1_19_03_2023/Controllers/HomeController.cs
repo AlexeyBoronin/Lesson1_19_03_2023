f666f944c1e4d703a869ce2f85d024e699761de3
Алексей <@DESKTOP-GDMIF0I>
2023-03-19 11:38:54 +0300

    Добавьте файлы проекта.

---
+++ Lesson1_19_03_2023/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lesson1_19_03_2023.Controllers
+{
+    public class HomeController:Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
